aaf68479d123408e65fee788791dc72d45f13542
ahjvdberg <ahjvdberg@hotmail.com>
2021-05-13 17:12:38 +0200

    test

--- mu-plugins/dimension_types.php
+++ mu-plugins/dimension_types.php
@@ -1,24 +1,6 @@
 
 <?php
 function dimension_types() {
-  // Event post type
-  register_post_type('event',array(
-    'show_in_rest' => true,
-    'capability_type' => 'event',
-    'map_meta_cap' => true,
-    'supports' => array('title','editor','excerpt'),
-    'rewrite' => array('slug' => 'events'),
-    'has_archive' => true,   
-    'public' => true,
-    'labels' => array(
-      'name' => 'Events',
-      'add_new_item' => 'Add New Event',
-      'edit_item' => 'Edit Event',
-      'all_items' => 'All Events',
-      'singular_name' => 'Event'
-    ),
-    'menu_icon' => 'dashicons-calendar'
-  ));
 
   // Program post type
   register_post_type('program',array(
